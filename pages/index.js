32aa8f1030996cb7cd6412fb5eabff5c48d84c71
muneeb-git-023 <muneeburrahman024>
2025-10-17 20:42:52 +0500

    Apni commit message

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>Welcome to My Next.js WebApp </h1>
-      <p>Deployed with CI/CD + Docker + Kubernetes + Azure</p>
+      <p>Deployed with CI/CD + Docker + Kubernetes </p>
     </div>
   );
 }
